fae6064537fb1693d9bca06949a80ba4eae5d56b
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-11-19 16:38:17 +0530

    removing calender

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Features from './Pages/Features/Features';
 import Landing from './Pages/Landing/InfoSection';
 import StartMeeting from './Pages/ActivityPage/Interview/StartMeeting';
 import JoinMeeting from './Pages/ActivityPage/Scedule/JoinMeeting';
-import MeetingPage from './Pages/MeetingPage'; // Add this import
 import VideoCallPage from './Pages/ActivityPage/VideoCallPage'
 
 function App() {
@@ -21,12 +20,6 @@ function App() {
           <Route path="/features" element={<Features />} />
           <Route path="/start-meeting" element={<StartMeeting />} />
           <Route path="/join-meeting" element={<JoinMeeting />} />
-          <Route path="/schedule" element={<Schedule />} />
-          
-          {/* Route for MeetingPage */}
-          <Route path="/meeting/:meetingCode" element={<MeetingPage />} />
-
-          {/* Route for VideoCallPage (Optional depending on design) */}
           <Route path="/meeting" element={<VideoCallPage />} />
         </Routes>
       </Router>
